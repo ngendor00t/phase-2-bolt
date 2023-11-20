18c2add8fb3a1810efea99ce0480f080bc95456a
Reginawambui <ngendoanita8@gmail.com>
2023-11-20 13:45:15 +0300

    fetching and displaying data

--- src/App.js
+++ src/App.js
@@ -1,43 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Botprofile from './components/Botprofile';
 
+function App() {
+  const [bots, setBots] = useState([]);
 
-function App(){
-  const[bots ,setBot]= useState([])
-  useEffect(()=>{
-    fetch("http://localhost:3000/bots")
-    .then (response=>response.json())
-    .then (data=>{
-      console.log(data)
-      setBot(data)  
-    })
-  }, [] )
-   
-    
+  useEffect(() => {
+    fetch('http://localhost:3000/bots')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setBots(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
   return (
-<>
-<Botprofile
-name=" "
-health=""
-damage=""
-avatar-url=""
-
-/>
-
-
-</>
- )
-  }
-
-
+    <div>
+      <h1>BotBattle App</h1>
+      {bots.length > 0 ? (
+        <Botprofile bots={bots} />
+      ) : (
+        <p></p>
+      )}
+    </div>
+  );
+}
 
 export default App;
-
-
-
-
-
--- src/components/Botprofile.js
+++ src/components/Botprofile.js
@@ -1,22 +1,23 @@
-import { useState } from "react" ;
+import React from "react";
 
-
-
-export default function Botprofile(){  
-
-
- const [bot , setBot]= useState(0);
- 
-return(
-<>
-<h1>BotBatrl: {bot}</h1>
-<button onClick={()=>setBot(+1)}>add</button>
-
-
-
-
-</>
-
-);
-
+export default function Botprofile({ bots }) {
+  return (
+    <>
+      <h1>BotBattle</h1>
+      <ul id="rows">
+    
+        {bots.map((bot) => (
+          <li key={bot.id} className="bot">
+            <img src={bot.avatar_url} alt="bot image" />
+            <p>name: {bot.name}</p> 
+            <p>health:{bot.health} </p>
+            <p>damage:{bot.damage}</p>
+            <p>armor:{bot.armor}</p>
+            <p>bot_class:{bot.class}</p>
+            <p>catchphrase:{bot.catchphrase}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
